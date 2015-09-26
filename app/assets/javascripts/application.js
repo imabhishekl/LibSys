b182dc30f59401075a5e8e16182e6a47c7709ca3
Abhishek <mail.abhisheklingwal@gmail.com>
2015-09-26 11:55:07 -0400

    Some helpful message for your future self

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ function ask_user_name(admin_user_name,isbn)
 	var u_name = prompt("Please enter the user_name for which checkout is requested")
 	if (u_name != null)
 	{
-		window.location= window.location.host + "/admin/checkout/" + admin_user_name + 
+		window.location= "librarysys.herokuapp.com" + "/admin/checkout/" + admin_user_name + 
 						"?u_name=" + u_name + "&isbn=" + isbn;
 	}
 }
@@ -27,7 +27,7 @@ function ask_user_name(admin_user_name,isbn)
 function warning_prompt(admin_user_name,u_name)
 {
 	alert("Warning please check if it has some currently checked out books!!!!!!!")
-		window.location= window.location.host + "/admin/delete_patrons/" + admin_user_name + 
+		window.location= "librarysys.herokuapp.com" + "/admin/delete_patrons/" + admin_user_name + 
 						"?u_name=" + u_name;
 }
 
@@ -35,7 +35,7 @@ function warning_prompt_admin_delete(admin_user_name,admin_u_name)
 {
 	if(confirm("Warning deleting a Admin!!!!!!!\nAre you sure"))
 	{
-		window.location= window.location.host + "/admin/delete_admin/" + admin_user_name + 
+		window.location= "librarysys.herokuapp.com" + "/admin/delete_admin/" + admin_user_name + 
 						"?admin_u_name=" + admin_u_name;	
 	}
 
@@ -44,6 +44,6 @@ function warning_prompt_admin_delete(admin_user_name,admin_u_name)
 function warning_prompt_book_delete(admin_user_name,isbn)
 {
 	alert("Warning deleting a books!!!!!!!")
-		window.location= window.location.host + "/book/delete_book/" + admin_user_name + 
+		window.location= "librarysys.herokuapp.com" + "/book/delete_book/" + admin_user_name + 
 						"?isbn=" + isbn;	
 }
