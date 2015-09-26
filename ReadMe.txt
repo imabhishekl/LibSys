a8df9702bcf8190541909785cde1a86299b130e7
Abhishek <mail.abhisheklingwal@gmail.com>
2015-09-26 02:33:23 -0400

    Changed Gemfile for Heroku

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+Admin Credential
+
+admin user name: boss@admin.com
+admin user pass: libsys@admin
+
+
+Bonus Credit Feature has not been implemented yet and we are planing to implement it in next 2 days
--- db/migrate/20150923221809_create_admin_user.rb
+++ db/migrate/20150923221809_create_admin_user.rb
@@ -1,6 +1,6 @@
 class CreateAdminUser < ActiveRecord::Migration
   def up
-	Admin.create(:user_name=>"imab",:name=>"abhishek",:password=>Base64.encode64("admin"),
+	Admin.create(:user_name=>"boss@admin.com",:name=>"abhishek",:password=>Base64.encode64("libsys@admin"),
 				:email=>"test@test.com",:primary_ind=>"Y")
   end
 end
