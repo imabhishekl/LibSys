370a8a7644ae64c64e6378a271e64f749dae92a5
Glen Menezes <gmeneze@ncsu.edu>
2015-10-12 21:34:18 -0400

    Edit form for new book requests

--- app/controllers/book_request_controller.rb
+++ app/controllers/book_request_controller.rb
@@ -20,6 +20,21 @@ class BookRequestController < ApplicationController
         redirect_to ("/user/show/" + session[:user_name])
   	end
 
+        def edit
+         puts "isbn is: " + params[:isbn]
+         @book_request=BookRequest.find_by_isbn(params[:isbn])
+        end
+
+        def update
+                puts "Glen isbn is: " + params[:book_request][:isbn] 
+                @book_request=BookRequest.find_by_isbn(params[:book_request][:isbn])
+        	if @book_request.update(book_params)
+                	redirect_to "/book/request_list/" + session[:user_name], notice: 'updated request details.'
+        	else
+                	redirect_to "/book/request_list/" + session[:user_name], notice: 'Failed to update details.'
+        	end
+        end
+
   	private
     	# Never trust parameters from the scary internet, only allow the white list through.
     	def book_params
